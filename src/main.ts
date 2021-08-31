30d2752a400a0f4c0f329fefdbf3ff970124ee38
lebrancconvas <lebrancconvas@gmail.com>
2021-08-31 21:00:11 +0700

    :sparkles: Learning Any and Unknown and Type and Interface.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-let elemental: string[] = ["Earth", "Water", "Wind", "Fire", "Light", "Dark", "Electric"] 
+let elemental: string[] = ["Earth", "Water", "Wind", "Fire", "Light", "Dark", "Electric"]
+interface Pokemon {
+	id: number;
+	name: string; 
+	element: string;
+	atk: number; 
+	def: number; 
+}
+let add = (a: number, b: number): number => {	
+	return a + b 	
+}
+
+
+type color = "blue" | "green" | "yellow"
+let a:color = "green"
+
+let expo = (a: number, b: number) => { 
+	return a.toString() + b.toString() 
+}
+
+console.log(expo(9, 9))   
